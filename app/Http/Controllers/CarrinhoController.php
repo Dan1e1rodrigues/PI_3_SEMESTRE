2376e93cb21a88ad76c8c42554d62bfc361b9007
Danillo Isaque Brito Lima <danilloisaq@gmail.com>
2023-04-17 14:18:03 -0300

    carrinho*
    falta arrumar as telas de login e cadastro , dashboard e fazer esse carrinho funcionar a partir do produto

--- app/Http/Controllers/CarrinhoController.php
+++ app/Http/Controllers/CarrinhoController.php
@@ -10,18 +10,19 @@ use illuminate\Support\Facades\Auth;
 class CarrinhoController extends Controller
 {
     public function store(Produto $produto){
-        
         $carrinho =Carrinho::create([
             'USUARIO_ID' => Auth::user()->USUARIO_ID,
             'PRODUTO_ID' => $produto->PRODUTO_ID,
             'ITEM_QTD' => 1
+
         ]);
-       
+
         return redirect(route('carrinho.index'));
     }
 
     public function index(){
         $carrinho = Carrinho::where('USUARIO_ID',Auth::user()->USUARIO_ID)->get();
-        return view ('carrinho.index')->with('carrinho',$carrinho);
+        return view ('carrinho.store')->with('carrinho',$carrinho);
+
     }
 }
