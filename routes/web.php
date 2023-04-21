b78baceb330d4b8583c73570b52aa33bd2494b68
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-04-21 19:50:32 -0300

    commit
    Chamando welcome

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/produto/{produto}', [ProdutoController::class, 'show'])->name('produto.show');
+
 require __DIR__.'/auth.php';
