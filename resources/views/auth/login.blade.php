94746ffcdd5dc17f215d62cc4546dc30599787d5
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-05-29 21:38:00 -0300

    commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,13 @@
-<link rel="stylesheet" href="http://127.0.0.1:8000/css/login.css">
-
-<x-guest-layout>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="http://127.0.0.1:8000/css/login.css">
+    <title>Delta</title>
+</head>
+<body>
+    
+<div>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -8,49 +15,43 @@
 
         @csrf
         
-<div class="login">
-
+        
+    <div class="login">
+        <div class="logo-yyt-logo-lo">
+            <img class="logo-yyt-logo-login" src="/img/logoOficialOficial.png">
+        </div>
         <!-- Email Address -->
         <div>
-            <label for="email">Email <label>
+            <label for="email">Email </label>
             <input class="inputLogin" type="email" name="email" :value="old('email')" required autofocus autocomplete="username">
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div>
-            <label for="password" :value="__('Password')">Senha </label>
+            <label for="password">Senha </label>
             <input id="password" class="inputLogin"
                             type="password"
                             name="password"
                             required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <!-- Remember Me -->
-        <div>
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="inputLogin" name="remember">
-                <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
+        <div class="senhmiy">
+
         </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
             <div>
                 <button class="buttonLogin">
-                    {{ __('Log in') }}
+                    login
                 </button>
+
+                <a class="link-yut" href="/register">
+                    Cadastrar
+                </a>
             </div>
         </div>
 
     </form>
 </div>
 
-</x-guest-layout>
+</div>
+</html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,27 +1,39 @@
-<!-- <x-guest-layout> -->
-@extends('layout.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="http://127.0.0.1:8000/css/cadastro.css">
+    <title>Cadastro</title>
+</head>
+<body>
+
+<div>
+
+
 <form method="POST" action="{{ route('register') }}">
+
     @csrf
 
-    <div class="telalogin">
+    <div class="login">
+        
         <h1>Cadastro</h1>
         <!--Campo input para colocar login/nome-->
 
         <div>
             <label for="nome">Nome</label>
-            <input type="text" placeholder="Digite seu nome" id="name" name="name" required>
+            <input class="inputLogin" type="text" placeholder="Digite seu nome" id="name" name="name" required>
         </div>
 
         <div>
         <!--Campo input para colocar senha-->
             <label for="nome">Email</label>
-            <input type="email" id="email" name="email" placeholder="Digite seu email">
+            <input class="inputLogin" type="email" id="email" name="email" placeholder="Digite seu email">
         </div>
 
         <div>
         <!-- Campo para colocar senha -->
             <label for="senha">Senha</label>
-            <input type="password" name="password" placeholder="Senha">
+            <input class="inputLogin" type="password" name="password" placeholder="Senha">
         </div>
 
         <!-- Esse campo não existe -->
@@ -29,26 +41,23 @@
 
         <div>
             <label for="Confirme sua senha">Confirme</label>
-            <input type="password" placeholder="Confirme sua senha" name="password_confirmation"/>
+            <input class="inputLogin" type="password" placeholder="Confirme sua senha" name="password_confirmation"/>
         </div>
 
         <div>
             <label for="CPF">CPF</label>
-            <input id="cpf" class="block mt-1 w-full" type="text" maxlength="11" name="cpf"/>
+            <input class="inputLogin" id="cpf" class="block mt-1 w-full" type="text" maxlength="11" name="cpf"/>
         </div>
 
-        <button class="ml-4">{{ __('Cadastrar') }}</button>
-        </div>
-        <br>
+        <button class="buttonLogin">{{ __('Cadastrar') }}</button>
+
         <p id="texto">Você já tem uma conta? <a href="/login">Fazer login</a></p>
+        </div>
+        
 
     </div>
 
+</div>
 
-
-
-
-
-
-</x-guest-layout>
-
+</body>
+</html>
