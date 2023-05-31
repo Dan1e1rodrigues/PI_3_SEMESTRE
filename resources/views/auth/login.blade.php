6d3598e3da9cc3f7168000c0328151885276ef33
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-05-31 20:37:39 -0300

    login & Cadastro
    Login e cadastro

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,86 +1,57 @@
-<link rel="stylesheet" href="http://127.0.0.1:8000/css/login.css">
-
-<<<<<<< Updated upstream
-<x-guest-layout>
-=======
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="http://127.0.0.1:8000/css/login.css">
+    <title>Delta</title>
+</head>
 <body>
-<!-- Estatus da sessão -->
-
->>>>>>> Stashed changes
+    
+<div>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
     <form method="POST" action="{{ route('login') }}">
+
         @csrf
-<<<<<<< Updated upstream
-<div class="funfa">>
+        
+        
+    <div class="login">
+        <div class="logo-yyt-logo-lo">
+            <img class="logo-yyt-logo-login" src="/img/logoOficialOficial.png">
+        </div>
         <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            <label for="email">Email </label>
+            <input class="inputLogin" type="email" name="email" :value="old('email')" required autofocus autocomplete="username">
         </div>
 
         <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-=======
-
-        <!-- Input de email -->
-        <div class="telaemail">
-
-            <p><label for="email">Email</label></p>
-            <p><input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username"></p>
-            <!-- Removido -->
-            <!-- <x-input-error :messages="$errors->get('email')" class="mt-2" /> -->
-
-        <!-- Input de senha -->
-            <p><label for="password">Senha</label></p>
-            <p><input id="password" class="block mt-1 w-full"
->>>>>>> Stashed changes
+        <div>
+            <label for="password">Senha </label>
+            <input id="password" class="inputLogin"
                             type="password"
                             name="password"
-                            required autocomplete="current-password"/></p>
-
-            <!-- <x-input-error :messages="$errors->get('password')" class="mt-2" /> -->
-
-<<<<<<< Updated upstream
-=======
-        <!-- Textoo lembrar senha -->
->>>>>>> Stashed changes
-        <!-- Remember Me -->
-        
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-       
-            <p></p>
-        <!-- <div class="flex items-center justify-end mt-4"> -->
-            @if (Route::has('password.request'))
-<<<<<<< Updated upstream
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-=======
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}" style="text-align:right;">
-                    {{ __('Esqueceu sua senha?') }}
->>>>>>> Stashed changes
-                </a>
-            @endif
-
-            <p></p>
-            <!-- Não remover esse "x-primary" pois ele manda para a tela de Redefinição de senha -->
-            <x-primary-button class="ml-3">
-                {{ __('Login') }}
-            </x-primary-button>
+                            required autocomplete="current-password" />
         </div>
-    </form>
-<<<<<<< Updated upstream
-</div>
-</x-guest-layout>
-=======
 
-</body>
->>>>>>> Stashed changes
+        <!-- Remember Me -->
+        <div class="senhmiy">
+
+        </div>
+            <div>
+                <button class="buttonLogin">
+                    login
+                </button>
+
+                <a class="link-yut" href="/register">
+                    Cadastrar
+                </a>
+            </div>
+        </div>
+
+    </form>
+</div>
+
+</div>
+</html>
