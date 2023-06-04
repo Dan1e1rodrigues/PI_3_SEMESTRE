f43450d5efa909633568de2f59964386f43faad4
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-06-03 21:48:05 -0300

    Tela de inserir endereço

--- resources/views/endereco/create.blade.php
+++ resources/views/endereco/create.blade.php
@@ -13,6 +13,8 @@
        }
      }
      </script>
+
+    <div class="principal-yt">
     <section>
         <table>
             <ol>
@@ -21,41 +23,42 @@
                       @csrf
                         <div>
                             <label for="">Nome(Ex:casa, trabalho e etc.)</label>
-                            <input class="" type="text" name="ENDERECO_NOME" required>
+                            <input class="inputLogin" type="text" name="ENDERECO_NOME" required>
                         </div>
 
                         <div>
                             <label for="">LOGRADOURO</label>
-                            <input class="" type="text" name="ENDERECO_LOGRADOURO" required>
+                            <input class="inputLogin" type="text" name="ENDERECO_LOGRADOURO" required>
                         </div>
 
                         <div>
                             <label for="">N° Residência<</label>
-                            <input class="" type="text" name="ENDERECO_NUMERO" required>
+                            <input class="inputLogin" type="text" name="ENDERECO_NUMERO" required>
                         </div>
 
                         <div>
                             <label for="">Complemento</label>
-                            <input class="" type="text" name="ENDERECO_COMPLEMENTO" required>
+                            <input class="inputLogin" type="text" name="ENDERECO_COMPLEMENTO" required>
                         </div>
 
                         <div>
                             <label for="">CEP</label>
-                            <input class="" type="text" name="ENDERECO_CEP" required minlength=8 maxlength=8 onkeypress="return semLetra(event)"> 
+                            <input class="inputLogin" type="text" name="ENDERECO_CEP" required minlength=8 maxlength=8 onkeypress="return semLetra(event)"> 
                         </div>
                         <div>
                             <label for="">Cidade</label>
-                            <input class="" type="text" name="ENDERECO_CIDADE" required>
+                            <input class="inputLogin" type="text" name="ENDERECO_CIDADE" required>
                         </div>
                         <div>
                             <label for="">Estado</label>
-                            <input class="" type="text" name="ENDERECO_ESTADO" required>
+                            <input class="inputLogin" type="text" name="ENDERECO_ESTADO" required>
                         </div>
-                      <button type="submit" class="btn btn-dark btn-block btn-lg"
-                        data-mdb-ripple-color="dark">Inserir</button>
+                      <button type="submit" class="button-nxy">Inserir</button>
                     </form>
                 </li>
             </ol>
         </table>
     </section>
+    </div>
+     
 @endsection
--- resources/views/pedido/show.blade.php
+++ resources/views/pedido/show.blade.php
@@ -6,7 +6,7 @@
 
 
 
-<section class="h-100 h-custom" style="background-color: #d2c9ff;">
+<section class="h-100 h-custom" style="background-color: #1C8394">
       <div class="container py-5 h-100">
         <div class="row d-flex justify-content-center align-items-center h-100">
           <div class="col-12">
@@ -16,7 +16,7 @@
                   <div class="col-lg-8">
                     <div class="p-5">
                       <div class="d-flex justify-content-between align-items-center mb-5">
-                        <h6>Obrigado por comprar na nossa loja!!</h6>
+                        <!-- <h6>Obrigado por comprar na nossa loja!!</h6> -->
                         <br>
                         <h1 class="fw-bold mb-0 text-black">Numero do Pedido:{{$pedido->PEDIDO_ID}}</h1>
                         <h6 class="mb-0 text-muted">Data da compra:{{$pedido->PEDIDO_DATA}}</h6>
@@ -30,6 +30,7 @@
                             src="{{$item->Produto->ProdutoImagem[0]->IMAGEM_URL}}"
                             class="img-fluid rounded-3" alt="Cotton T-shirt">
                         </div>
+                        
                         <div class="col-md-3 col-lg-3 col-xl-3">
                           <h6 class="text-black mb-0">{{$item->Produto->PRODUTO_NOME}}</h6>
                         </div>
@@ -53,6 +54,7 @@
                         </div>
                           @else
                           <h6 class="mb-0">{{$item->Produto->PRODUTO_PRECO * $item-> ITEM_QTD}}</h6>
+                          <h6 class="mb-0 text-muted">Data da compra:{{$pedido->PEDIDO_DATA}}</h6>
                           @endif
                         <div class="col-md-1 col-lg-1 col-xl-1 text-end">
                           <a href="#!" class="text-muted"><i class="fas fa-times"></i></a>
@@ -64,7 +66,7 @@
 
                   <div class="col-lg-4 bg-grey">
                     <div class="p-5">
-                      <h3 class="fw-bold mb-5 mt-2 pt-1">Summary</h3>
+                      <h3 class="fw-bold mb-5 mt-2 pt-1">Resumo</h3>
                       <hr class="my-4">
 
                       <div class="d-flex justify-content-between mb-4">
@@ -95,11 +97,12 @@
                         <h5>R${{$preco}}</h5>
                       </div>
 
-                      <div class="d-flex justify-content-between mb-4">
-                      <a href="/"> <button type="submit" class="btn btn-dark btn-block btn-lg"
-                        data-mdb-ripple-color="dark">Sair</button>
-                      </a>
                       </div>
+                      <div class="d-flex justify-content-between mb-4">
+                      <a href="/"> 
+                        <button type="submit" class="buttonfinalizar">Sair</button>
+                      </a>
+
                     </div>
                   </div>
                 </div>
